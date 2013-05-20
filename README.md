55c5b3d7dd361c06d5cde939766d1f86776cb6d2
Tero Kujala <faramir_5001@hotmail.com>
2013-05-20 14:06:49 +0300

    uus alustus

---
+++ README.md
@@ -0,0 +1,14 @@
+kantoliina
+==========
+
+Ohjelmistotuotantoprojekti
+
+Backlogit ja työtunnit: https://drive.google.com/folderview?id=0B6bQbGiOmqPMQ25XcmdiRHRZRE0&usp=sharing
+
+Katseltavana:
+-backlogit
+
+https://docs.google.com/spreadsheet/ccc?key=0AqbQbGiOmqPMdHU3QXhWNUlCRnBxcGEzeEhsQ1FNZEE&usp=sharing
+
+-työtunnit
+https://docs.google.com/spreadsheet/ccc?key=0AqbQbGiOmqPMdFdrYklDU3JIbldMVnhTX09Yb2RPanc&usp=sharing
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kantoliina::Application.config.secret_token = '544aed5199903630808a2d5e1ed3ecedd7e4c7a219e2d3e9710b583b12dd742e743e972285eedad956a106a4dfb11ccc586cb533ac7d9997fc9eb736b6219b6d'
