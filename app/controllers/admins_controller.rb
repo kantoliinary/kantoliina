8c0254eb95ed931725f3250827ae50a64e3c1dd6
Tero Kujala <faramir_5001@hotmail.com>
2013-05-21 13:35:48 +0300

    admin kirjautuminen

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,8 +1,16 @@
 class AdminsController < ApplicationController
   def loginform
-
+    @error = Hash.new
   end
   def login
-    puts "jee"
+    user = Admin.find_by_login(params[:login])
+    @error = Hash.new
+    if  user && user.password == params[:password]
+      session[:id] = user.id
+      flash[:notice] = "Kirjauduttu"
+      redirect_to login_path and return
+    end
+    @error[:error] = "Virheellinen kayttajatunnus tai salasana"
+    render "loginform"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Kantoliina::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
-
+  get 'login' => 'admins#loginform'
+  post 'login' => 'admins#login'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
