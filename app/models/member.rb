aa7f7daa35ac292516f8667662f4c89c43094da7
Kalle J Lammenoja <lammenoj@dcb-24.cs.helsinki.fi>
2013-05-23 09:55:57 +0300

    tietokantaan muutoksia

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :surname, :municipality, :address, :zipcode, :postoffice, :email, :membergroup, :membernumber, :payday
 
 
 end
--- db/migrate/20130521105651_member.rb
+++ db/migrate/20130521105651_member.rb
@@ -2,6 +2,15 @@ class Member < ActiveRecord::Migration
   def change
     create_table :members do |t|
       t.string :name
+      t.string   :surname
+      t.string   :municipality
+      t.string   :address
+      t.string   :zipcode
+      t.string   :postoffice
+      t.string   :email
+      t.string   :membergroup
+      t.integer  :membernumber
+      t.date     :payday
       t.timestamps
     end
   end
