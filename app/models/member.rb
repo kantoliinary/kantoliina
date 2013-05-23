f502c572e2ef1a5e198f60a173721f38bb654ab3
Taneli Virkkala <taneli.virkkala@helsinki.fi>
2013-05-23 10:36:32 +0300

    jasen validioitu

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,13 @@
 class Member < ActiveRecord::Base
   attr_accessible :name, :surname, :municipality, :address, :zipcode, :postoffice, :email, :membergroup, :membernumber, :payday
-
-
+  validates :name, :presence => true
+  validates :surname, :presence => true
+  validates :municipality, :presence => true
+  validates :address, :presence => true
+  validates :zipcode, :presence => true
+  validates :postoffice, :presence => true
+  validates :email, :presence => true
+  validates :membergroup, :presence => true
+  validates :membernumber, :presence => true
+  validates :payday, :presence => true
 end
