f8dbf399ad274c2a764b6c3c17878426bb38be6b
Timo <timo.o.pitkanen@cs.helsinki.fi>
2013-05-23 10:34:51 +0300

    tests added

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -2,7 +2,16 @@ require 'faker'
 
 FactoryGirl.define do
   factory :member do |f|
-    f.name "admin"
+    f.name "John"
+    f.surname "Doe"
+    f.municipality "f"
+    f.zipcode "00540"
+    f.postoffice "Helsinki"
+    f.email "john@doe.com"
+    f.membergroup "varsinaisjasen"
+    f.membernumber "333"
+    f.payday "21/12/12"
 
   end
 end
+
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -6,6 +6,42 @@ describe Member do
     FactoryGirl.create(:member).should be_valid
   end
 
+  it "is invalid without a name" do
+    FactoryGirl.build(:member, name: nil).should_not be_valid
+  end
+
+  it "is invalid without a surname" do
+    FactoryGirl.build(:member, surname: nil).should_not be_valid
+  end
+
+  it "is invalid without a municipality" do
+    FactoryGirl.build(:member, municipality: nil).should_not be_valid
+  end
+
+  it "is invalid without a zipcode" do
+    FactoryGirl.build(:member, zipcode: nil).should_not be_valid
+  end
+
+  it "is invalid without a postoffice" do
+    FactoryGirl.build(:member, postoffice: nil).should_not be_valid
+  end
+
+  it "is invalid without a email" do
+    FactoryGirl.build(:member, email: nil).should_not be_valid
+  end
+
+  it "is invalid without a membergroup" do
+    FactoryGirl.build(:member, membergroup: nil).should_not be_valid
+  end
+
+  it "is invalid without a membernumber" do
+    FactoryGirl.build(:member, membernumber: nil).should_not be_valid
+  end
+
+  it "is invalid without a payday" do
+    FactoryGirl.build(:member, payday: nil).should_not be_valid
+  end
+
 
 
 end
