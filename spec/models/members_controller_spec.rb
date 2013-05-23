a8073d7cf961d3a8d40d0a542f119928f532dbf3
Taneli Virkkala <taneli.virkkala@helsinki.fi>
2013-05-23 13:35:14 +0300

    testeja tehty

---
+++ spec/models/members_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+
+describe members_controller do
+
+ # describe "POST create" do
+  #  context "with valid attributes" do
+    #  it "creates a new member" do
+     #   expect{
+     #     post :create, members_controller: Factory.attributes_for(:members_controller)
+    #    }.to change(MembersController,:count).by(1)
+    #  end
+   # end
+   # end
+
+  #describe 'members with validate values' do
+   # it 'should call find with right name' do
+     # Member.should_receive(:find).eql?("John").and_return(Member.new({:name => "John"}))
+     # get :find_all_by_name {:name => 'John'}
+    #end
+    #it 'should call find with right surname' do
+     # Member.should_receive(:find).eql?("Doe").and_return(Member.new({:surname => "Doe"}))
+    #  get :find_all_by_surname {:surname => 'Doe'}
+    #end
+   # it 'should call find with right municipality' do
+      #Member.should_receive(:find).eql?("f").and_return(Member.new({:municipality => "f"}))
+     # get :find_all_by_municipality {:municipality => 'f'}
+    #end
+   # it 'should call find with right zipcode' do
+      #Member.should_receive(:find).eql?("00540").and_return(Member.new({:zipcode => "00540"}))
+     # get :find_all_by_zipcode {:municipality => '00540'}
+    #end
+    #it 'should call find with right postoffice' do
+     # Member.should_receive(:find).eql?("Helsinki").and_return(Member.new({:postoffice => "Helsinki"}))
+    #  get :find_all_by_zipcode {:postoffice => 'Helsinki'}
+   # end
+  #end
+end
