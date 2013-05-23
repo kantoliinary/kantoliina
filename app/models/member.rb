7962b2efbf0e998f47e10053d8eab375c04c49d9
Tero Kujala <faramir_5001@hotmail.com>
2013-05-23 16:52:10 +0300

    member cucumber testit

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,13 +10,13 @@ class Member < ActiveRecord::Base
             #    :message => "Sukunimen muoto ei kelpaa!"}
   validates :municipality, :presence => {:message => "Kunta puuttuu!"}
   validates :address, :presence => {:message => "Osoite puuttuu!"}
-  validates :zipcode, :length => {:minimum => 5,  :maximum => 5, :message => "Postinumeron tulee olla viiden merkin pituinen!"}, :numericality => {:only_integer => {:message => "Postinumeron tulee sisaltaa vain numeroita!"}}, :presence => {:message => "Postinumero puuttuu!"}
+  validates :zipcode, :presence => {:message => "Postinumero puuttuu!"}, :length => {:minimum => 5,  :maximum => 5, :message => "Postinumeron tulee olla viiden merkin pituinen!"}, :numericality => {:only_integer => true, :message => "Postinumeron tulee sisaltaa vain numeroita!"}
   validates :postoffice, :presence => {:message => "Postitoimipaikka puuttuu!"}
   validates :email, :presence => {:message => "Sahkopostiosoite puuttuu!"},
             :format => {
                 :with    => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
                 :message => "Sahkopostiosoitteen muoto on vaara!"}
   validates :membergroup, :presence => true
-  validates :membernumber, :numericality => {:only_integer => {:message => "Jasennumerossa tulee olla vain numeroita!"}}, :length => {:minimum => 3,  :maximum => 19, :message => "Jasennumeron tulee olla 3-19 merkkia pitka!"}, :presence => {:message => "Jasennumero puuttuu!"}
+  validates :membernumber, :presence => {:message => "Jasennumero puuttuu!"}, :numericality => {:only_integer => {:message => "Jasennumerossa tulee olla vain numeroita!"}}, :length => {:minimum => 3,  :maximum => 19, :message => "Jasennumeron tulee olla 3-19 merkkia pitka!"}
   validates :payday, :presence => {:message => "Viimeinen maksupaiva puuttuu!"}
 end
