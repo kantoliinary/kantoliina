e838f3e8cf28ca3f9cdcc28b1977e046c1153bd3
Taneli Virkkala <taneli.virkkala@helsinki.fi>
2013-05-27 13:55:01 +0300

    delete testia tehty

---
+++ features/step_definitions/delete_member_steps.rb
@@ -0,0 +1,6 @@
+When /^I select "([^"]*)" as the (.+) "([^"]*)" date$/ do |date, model, selector|
+  date = Date.parse(date)
+  select(date.year.to_s, :from => "#{model}[#{selector}(1i)]")
+  select(date.strftime("%B"), :from => "#{model}[#{selector}(2i)]")
+  select(date.day.to_s, :from => "#{model}[#{selector}(3i)]")
+end
