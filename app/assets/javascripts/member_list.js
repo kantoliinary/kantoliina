5d928d0ce36296b4472deb13b09ee0d5482aa7e8
Tero Kujala <faramir_5001@hotmail.com>
2013-05-29 12:00:52 +0300

    kaikkien checkboxien valinta

--- app/assets/javascripts/member_list.js
+++ app/assets/javascripts/member_list.js
@@ -1,20 +1,28 @@
-$(document).ready(function (){
-    $("#index_member_page #send_invoice").click(function (e){
+$(document).ready(function () {
+    var index = $("#index_member_page")
+    index.find("#send_invoice").click(function (e) {
         e.preventDefault()
         var checkboxs = $("#index_member_page #members").find(":checkbox")
         var ids = []
-        $(checkboxs).each(function (index, value){
+        $(checkboxs).each(function (index, value) {
             var checkbox = $(value);
-            if(checkbox.is(":checked") && checkbox.attr("name") != "check_all"){
+            if (!!checkbox.attr("checked") && checkbox.attr("name") != "check_all") {
                 ids.push(checkbox.val())
             }
         });
-        var object = {
-            ids: ids
-        }
-        var json = JSON.stringify(object)
         var form = $("#index_member_page #invoice_form")
-        form.children("#ids").val(json)
-        form.submit();
+        form.children("#ids").val(JSON.stringify({
+            ids: ids
+        }))
+        if (ids.length != 0) {
+            form.submit();
+        }
+    })
+    index.find("#check_all").click(function () {
+        var checkboxs = index.children("#members").find(":checkbox")
+        var check_state = !!$(this).attr("checked");
+        $(checkboxs).each(function (index, value) {
+            $(value).attr("checked", check_state)
+        })
     })
 })
