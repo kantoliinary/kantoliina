528a238bb0795a9ad37277c69587bf301612eff3
Tero Kujala <faramir_5001@hotmail.com>
2013-06-04 12:41:52 +0300

    salasanan palautus

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,14 +5,14 @@
 # Validates the presence and length of the username and password.
 
 class Admin < ActiveRecord::Base
-  attr_accessible :username, :password, :password_digest
+  attr_accessible :username, :password, :password_digest, :email
   has_secure_password
   validates :password, :username, :presence => true
   validates :username, :length => {
-    :minimum => 3,
-    :maximum => 20,
-    :too_short => "Käyttäjätunnuksen tulee olla vähintään 3 merkin pituinen",
-    :too_long => "Käyttäjätunnuksen tulee olla korkeintaan 20 merkin pituinen"
+      :minimum => 3,
+      :maximum => 20,
+      :too_short => "Käyttäjätunnuksen tulee olla vähintään 3 merkin pituinen",
+      :too_long => "Käyttäjätunnuksen tulee olla korkeintaan 20 merkin pituinen"
   }
   validates :password, :length => {
       :minimum => 8,
@@ -20,4 +20,20 @@ class Admin < ActiveRecord::Base
       :too_short => "Salasanan tulee olla vähintään 8 merkin pituinen",
       :too_long => "Salasanan tulee olla korkeintaan 20 merkin pituinen"
   }
+  validates :email, :presence => {:message => "Sähköpostiosoite puuttuu!"},
+            :format => {
+                :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+                :message => "Sähköpostiosoitteen muoto on väärä!"}
+  def generate_and_send_new_password
+    new_password = ""
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#,.;:?%&".split(//)
+    (0..8).each do |i|
+      new_password += chars.at(rand(chars.length))
+    end
+    puts new_password
+    self.update_attributes(:password => new_password)
+    save!
+    AdminMailer.password_reset(self, new_password).deliver
+    new_password
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(:version => 20130603080112) do
   create_table "admins", :force => true do |t|
     t.string   "username"
     t.string   "password_digest"
+    t.string   "email"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
