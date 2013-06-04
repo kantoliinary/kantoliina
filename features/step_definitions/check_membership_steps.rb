d7f9a9ed19bea36016df0925e10d27ef1cc97f15
Kalle J Lammenoja <lammenoj@dcb-24.cs.helsinki.fi>
2013-06-04 11:05:13 +0300

    kurkkua partnerille

---
+++ features/step_definitions/check_membership_steps.rb
@@ -0,0 +1,18 @@
+#Given /the following partners exist/ do |partners_table|
+#  partners_table.hashes.each do |partner|
+#    Partner.create!(partner)
+#  end
+#end
+#
+#Given /the following membergroups exist/ do |membergroups_table|
+#  membergroups_table.hashes.each do |membergroup|
+#    Membergroup.create!(membergroup)
+#  end
+#end
+
+#Given /the following members exist/ do |members_table|
+#  members_table.hashes.each do |member|
+#    Member.create!(member)
+#  end
+#end
+
