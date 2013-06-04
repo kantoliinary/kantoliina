a4019a31ef20606e66ac7b89173ce2c7c2a647ef
Timo <timo.o.pitkanen@cs.helsinki.fi>
2013-06-04 13:19:27 +0300

    testei

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -16,6 +16,14 @@ describe PasswordResetsController do
       post :create, FactoryGirl.attributes_for(:admin)
       response.should redirect_to login_path
     end
+    it "password resets" do
+      admin = FactoryGirl.build(:admin)
+      Admin.stub(:find_by_email).and_return(admin)
+
+      post :create, FactoryGirl.attributes_for(:admin)
+      admin.password != "qwerty123"
+      response.should redirect_to login_path
+    end
   end
 
 end
