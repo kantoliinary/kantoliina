d88cf2cf6d6faef27277339d7839777a24d8c528
Tero Kujala <faramir_5001@hotmail.com>
2013-06-06 11:12:40 +0300

    laskupohja editor

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,11 @@
 class SettingsController < ApplicationController
 
   def index
-
+    @template = ""
+    File.open(Rails.root.join("app", "views", "billing", "bill_email.html.haml").to_s, 'r') do |f|
+      while line = f.gets
+        @template += line
+      end
+    end
   end
 end
