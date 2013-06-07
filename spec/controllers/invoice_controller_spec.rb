7b39f17544d4300e2e4bb1d7ac0c25d9d6a58b0e
Taneli Virkkala <taneli.virkkala@helsinki.fi>
2013-06-07 15:28:00 +0300

    herokuun

--- spec/controllers/invoice_controller_spec.rb
+++ spec/controllers/invoice_controller_spec.rb
@@ -11,10 +11,9 @@ describe InvoiceController do
 
   describe "POST #index" do
     it 'finds members with given ids' do
-
       FactoryGirl.create(:membergroup)
       post :index, :ids => "{\"ids\":[\"1\"]}"
-
+      response.should render_template :index
     end
   end
 
@@ -23,17 +22,12 @@ describe InvoiceController do
     context "with valid attributes" do
       it "mail will be created" do
         FactoryGirl.create(:membergroup)
-
         member = FactoryGirl.create(:member)
         member2 = FactoryGirl.create(:member, membernumber: 54321, id: 2)
         members = [member, member2]
         Member.stub(:find_all_by_id).and_return(members)
-
         post :create, :additional_message => "fa"
-        response.should_not be_success
-
         response.should redirect_to members_path
-
       end
     end
   end
@@ -46,18 +40,14 @@ describe InvoiceController do
       file.should_receive(:puts).with("text")
       post :update, :template => "text"
       response.should redirect_to settings_path
-
     end
   end
 
   it "should not update e-mail with invalid row" do
     file = mock('file')
     File.stub(:open).with(Rails.root.join("app", "views", "billing", "bill_email.html.haml").to_s, "w").and_yield(file)
-
     post :update, :template => "             text"
     flash[:error].should include ("Virheellinen sisennys rivillä")
-
-
   end
 
   it "should work with a line starting with %" do
@@ -66,8 +56,6 @@ describe InvoiceController do
     file.should_receive(:puts).with("%br")
     post :update, :template => "%br"
     response.should redirect_to settings_path
-
-
   end
 end
 
