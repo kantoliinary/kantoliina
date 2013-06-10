fcc319218aa221c543243cb0ffec9d04a0c5e745
Kalle J Lammenoja <lammenoj@dcb-24.cs.helsinki.fi>
2013-06-10 15:40:51 +0300

    validointia

--- app/models/membergroup.rb
+++ app/models/membergroup.rb
@@ -8,5 +8,5 @@ class Membergroup < ActiveRecord::Base
   has_many :members
 
   validates :name, :uniqueness => {:message => "Jäsenryhmä on jo olemassa"}, :presence => {:message => "Ryhmän nimi puuttuu"}
-  validates :fee, :presence => {:message => "Hinta puuttuu"}, :numericality => {:only_integer => true, :message => "tulee olla numero"}
+  validates :fee, :presence => {:message => "Hinta puuttuu"}, :numericality => {:message => "tulee olla numero"}
 end
