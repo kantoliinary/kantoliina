b45c8a5b47c5e0b578042b982b9259421911ae49
Eero Laine <eerovolaine@yahoo.com>
2013-06-10 11:51:05 +0300

    z

--- app/controllers/membergroups_controller.rb
+++ app/controllers/membergroups_controller.rb
@@ -4,4 +4,19 @@ class MembergroupsController < ApplicationController
     @membergroups = Membergroup.all
   end
 
+  def edit
+    @membergroup = flash[:membergroup] || Membergroup.find(params[:id])
+    @submit_text = "Tallenna muutokset"
+  end
+
+  def update
+    @membergroup = Membergroup.find(params[:id])
+    if @membergroup.update_attributes(params[:membergroup])
+      flash[:notice] = "Tiedot muutettu"
+    else
+      flash[:membergroup] = @membergroup
+    end
+    redirect_to edit_membergroup_path
+  end
+
 end
