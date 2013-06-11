a2a59ed321a87277bfbe3ed7f0777c7a980cca56
Eero Laine <eerovolaine@yahoo.com>
2013-06-11 11:10:47 +0300

    testit jäsenryhmien lisäykselle

--- features/step_definitions/edit_member_steps.rb
+++ features/step_definitions/edit_member_steps.rb
@@ -1,3 +1,3 @@
-Then /^I should see the image "(.+)"$/ do |image|
-  page.should have_xpath("//img[@src=\"/public/images/#{image}\"]")
-end
+#Then /^I should see the image "(.+)"$/ do |image|
+#  page.should have_xpath("//img[@src=\"/public/images/#{image}\"]")
+#end
