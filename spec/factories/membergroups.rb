c02f0951b2518a2329a35aec7ba90d6624da8d0a
Kalle J Lammenoja <lammenoj@dcb-24.cs.helsinki.fi>
2013-06-11 12:09:39 +0300

    membergroups rspec testit

--- spec/factories/membergroups.rb
+++ spec/factories/membergroups.rb
@@ -6,5 +6,19 @@ FactoryGirl.define do
     f.id "1"
     f.name "Varsinainen jasen"
     f.fee "10.00"
+    f.onetimefee true
   end
+
+  #factory :invalid_name_membergroup do |f|
+  #  f.id "2"
+  #  f.name nil
+  #  f.fee "jee"
+  #end
+  #
+  #factory :invalid_fee_membergroup do |f|
+  #  f.id "3"
+  #  f.name "jäsenryhmä"
+  #  f.fee "jee"
+  #end
+
 end
--- spec/models/membergroup_spec.rb
+++ spec/models/membergroup_spec.rb
@@ -6,11 +6,16 @@ describe Member do
     FactoryGirl.create(:membergroup).should be_valid
   end
 
-  #it "is invalid without a name" do
-  #  FactoryGirl.build(:invalid_name_membergroup).should_not be_valid
-  #end
-  #
-  #it "is invalid with text as fee" do
-  #  FactoryGirl.build(:invalid_fee_membergroup).should_not be_valid
-  #end
+  it "is invalid with existing name" do
+    FactoryGirl.create(:membergroup)
+    FactoryGirl.build(:membergroup).should_not be_valid
+  end
+
+  it "is invalid without a name" do
+    FactoryGirl.build(:membergroup, name: nil).should_not be_valid
+  end
+
+  it "is invalid with text as fee" do
+    FactoryGirl.build(:membergroup, fee: "moi").should_not be_valid
+  end
 end
