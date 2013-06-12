57e5954b5616d6384a64e62b90fdf2523297c6b5
Timo <timo.o.pitkanen@cs.helsinki.fi>
2013-06-12 10:09:09 +0300

    karhua

--- app/mailers/billing.rb
+++ app/mailers/billing.rb
@@ -10,7 +10,10 @@ class Billing < ActionMailer::Base
     mail(:to => member.email, :subject => "Kantoliinayhdistyksen jäsenmaksu - lasku")
   end
 
-  def reminder_email member
+  def reminder_email member, additional_message
+    additional_message_split = additional_message.split(/^-+\s*$/)
+    @top_additional_message = additional_message_split.at(0)
+    @bottom_additional_message = additional_message_split.at(1)
 
     @member = member
     mail(:to => member.email, :subject => "Kantoliinayhdistyksen jäsenmaksu - Maksumuistutus")
