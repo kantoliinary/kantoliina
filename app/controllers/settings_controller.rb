b3205422eeb44490a80bab34a411acbba5102295
Kalle J Lammenoja <lammenoj@dcb-24.cs.helsinki.fi>
2013-06-13 13:13:49 +0300

    jotain incoice juttui

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -12,7 +12,13 @@ class SettingsController < ApplicationController
     @error = flash[:error] || ""
     @errorline = flash[:errorline] || 0
 
-    template = EditorHelper.index
+    @template = flash[:template] || File.open(Rails.root.join("app", "views", "billing", "bill_email.html.haml").to_s, 'r') do |f|
+      template = ""
+      while line = f.gets
+        template += line
+      end
+      template
+    end
     end
 
 
