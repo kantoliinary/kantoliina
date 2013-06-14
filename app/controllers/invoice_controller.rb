7865c18ab78bc0e6c3bec61505294c9efa65d2e4
Kalle J Lammenoja <lammenoj@dcb-24.cs.helsinki.fi>
2013-06-14 12:49:32 +0300

    editorissa esikatselu korjattu

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -32,6 +32,21 @@ class InvoiceController < ApplicationController
     render "settings/invoice_edit"
   end
 
+
+  def load_default
+
+    template = ""
+    File.open(Rails.root.join("app", "views", "billing", "default_bill.html.haml").to_s, 'r') do |f|
+      while line = f.gets
+        template += line
+      end
+    end
+
+    flash[:template] = template
+
+    redirect_to  invoice_edit_path
+  end
+
   ##
   # Selects a group of members by chosen ID and sends an invoice to their e-mails.
   def create
