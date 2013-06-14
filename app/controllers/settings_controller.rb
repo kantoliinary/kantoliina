f148ce502f46d9fb5b45a1e149bb1834b23c5959
Kalle J Lammenoja <lammenoj@dcb-24.cs.helsinki.fi>
2013-06-14 13:27:57 +0300

    laskun esikatseluun validointi check

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -10,23 +10,6 @@ class SettingsController < ApplicationController
   end
 
 
-  def load_default
-    template = ""
-
-
-    File.open(Rails.root.join("app", "views", "billing", "default_bill.html.haml").to_s, 'r') do |f|
-      while line = f.gets
-        template += line
-      end
-    end
-
-    flash[:template] = template
-
-    redirect_to settings_path
-
-
-  end
-
   private
 
   ##
