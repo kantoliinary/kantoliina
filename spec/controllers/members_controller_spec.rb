97904d524f4f51e10df235a0fb40feb277aa52b3
Tero Kujala <faramir_5001@hotmail.com>
2013-06-17 16:00:08 +0300

    rspeckit

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -37,9 +37,7 @@ describe MembersController do
     context "with valid attributes" do
       it "member will be created" do
         FactoryGirl.create(:membergroup)
-        post :create, :member => {:id => "1", :firstnames => "joku", :surname => "jokinen", :municipality => "helsinki", :zipcode => "12346",
-                                  :address => "puutie", :postoffice => "stadi", :email => "jokin@jotain.com", :membergroup_id => 1, :membernumber => "54321",
-                                  :membershipyear => "2014", :paymentstatus => "f", :invoicedate => "08/08/2013", :active => "t"}, :nextyear => true, :sendinvoice => true
+        post :create, :member => {:firstnames => "John", :surname => "Doe", :municipality => "f", :zipcode => "00540", :address => "street 7", :postoffice => "Helsinki", :country => "Suomi", :email => "sgafd@gmaik.com", :membergroup_id => "1", :membernumber => "33333", :membershipyear => "2014", :paymentstatus => "f", :invoicedate => "08/08/2013", :active => "t", :lender => "f", :support => "f"}, :nextyear => true, :sendinvoice => true
         flash[:notice].should == "Jäsen lisätty"
       end
     end
