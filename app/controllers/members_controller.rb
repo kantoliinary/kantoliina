873ef9d2f6fb742cb9cbd2bdf6137999bda62fd3
Tero Kujala <faramir_5001@hotmail.com>
2013-06-17 14:36:23 +0300

    poistettu valikon korjaus

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -184,9 +184,8 @@ class MembersController < ApplicationController
       end
     end
 
-    if filters[:deleted] && filters[:deleted].length == 1
-      members = members.where(:deleted => filters[:deleted].at(0) == "1")
-    end
+    members = members.where(:deleted => !!filters[:deleted])
+
     if filters[:paymentstatus] && filters[:paymentstatus].length == 1
       members = members.where(:paymentstatus => filters[:paymentstatus].at(0) == "1")
     end
