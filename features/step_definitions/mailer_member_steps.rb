24eeaefef778b5af6110ea6bc30a27ebe0bb9ac1
Taneli Virkkala <taneli.virkkala@helsinki.fi>
2013-06-19 10:39:47 +0300

    testia mailille

---
+++ features/step_definitions/mailer_member_steps.rb
@@ -0,0 +1,9 @@
+When /^I upload a file with valid data for 3 new products$/ do
+  attach_file(:csv_file, File.join('features', 'upload-files', 'products_csv_ok.csv'))
+  click_button "Send file"
+end
+
+When /^I upload an exact file$/ do
+  attach_file(:attachment, File.join('features', 'support', 'env.rb'))
+  #click_button "Browse..."
+end
