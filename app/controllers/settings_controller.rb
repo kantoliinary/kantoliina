fb7e81badf910eeca92290dcc6b9b2bf8dbf1bb2
Eero Laine <eerovolaine@yahoo.com>
2013-06-19 15:41:13 +0300

    more rdoc

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,9 +1,10 @@
 #encoding: utf-8
 ##
-# The controller for the
+# The controller for the settings page
 class SettingsController < ApplicationController
 
-
+  ##
+  # The default index method
   def index
 
   end
